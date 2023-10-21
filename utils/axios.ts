a7e7b46e9b864ab3fdcf030339aae75ef1653290
zhalok <zhalokrahman007@gmail.com>
2023-10-22 01:12:00 +0600

    using the backend url from env

--- utils/axios.ts
+++ utils/axios.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export default axios.create({
-  baseURL: "http://localhost:8000",
+  baseURL: process.env.NEXT_PUBLIC_BACKEND_BASE,
   headers: {
     "Content-type": "application/json",
   },
